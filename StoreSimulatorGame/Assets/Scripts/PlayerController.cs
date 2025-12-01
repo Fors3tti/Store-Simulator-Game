97c7429f6c9f4ef20fba3cb54c14a24f5da02727
fors3tti <152114883+Fors3tti@users.noreply.github.com>
2025-12-01 13:05:48 -0400

    Added Players Move Configs

--- StoreSimulatorGame/Assets/Scripts/PlayerController.cs
+++ StoreSimulatorGame/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     public InputActionReference moveAction;
+    public float moveSpeed;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -14,6 +15,11 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 moveInput = moveAction.action.ReadValue<Vector2>();
+
+        //Debug.Log(moveInput);
+
+        transform.position = transform.position + new Vector3(
+            moveInput.x * Time.deltaTime * moveSpeed, 0f, moveInput.y * Time.deltaTime * moveSpeed);
     }
 }
