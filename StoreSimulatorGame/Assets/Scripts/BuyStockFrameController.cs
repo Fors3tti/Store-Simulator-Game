c791495c090f248c208525b7641d44d7c9a6e708
fors3tti <152114883+Fors3tti@users.noreply.github.com>
2026-03-24 01:27:11 -0400

    Updated Box Stock Menu Configs

--- StoreSimulatorGame/Assets/Scripts/BuyStockFrameController.cs
+++ StoreSimulatorGame/Assets/Scripts/BuyStockFrameController.cs
@@ -7,15 +7,35 @@ public class BuyStockFrameController : MonoBehaviour
 
     public TMP_Text nameText, priceText, boxAmountText, boxPriceText, buttonText;
 
+    public StockBoxController boxToSpawn;
+
+    private float boxCost;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        UpdateFrameInfo();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void UpdateFrameInfo()
+    {
+        info = StockInfoController.instance.GetInfo(info.name);
+
+        nameText.text = info.name;
+        priceText.text = "R$" + info.price.ToString("F2");
+
+        int boxAmount = boxToSpawn.GetStockAmount(info.typeOfStock);
+        boxAmountText.text = boxAmount.ToString() + " per box";
+
+        boxCost = boxAmount * info.price;
+        boxPriceText.text = "Box: R$" + boxCost.ToString("F2");
+
+        buttonText.text = "PAY: R$" + boxCost.ToString("F2");
     }
 }
